916f51d090184b10daf71af1e6b19120bac92419
unknown <patidaranil0791@gmail.com>
2022-03-14 16:10:19 +0530

    OTP VERIFY

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import StepForm from './component/stepForm';
+import auth from './auth';
+import Home from './component/home';
+
+function App() {
+  if (auth.isAuthenticated()) return <Home />;
+	else return <StepForm />;
+}
+
+export default App;
